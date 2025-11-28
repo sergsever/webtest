1b7b4553e370101b6618bf1f2713b97b4529f400
sergey.lvov70 <sergey.lvov70@gmail.com>
2025-11-28 12:24:25 +0300

    security config

---
+++ src/main/java/com/example/webtest/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.webtest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/", "/cards").permitAll()
+				.anyRequest().authenticated()
+			)
+			/*.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()*/
+			
+			.logout((logout) -> logout.permitAll())
+			.httpBasic(Customizer.withDefaults());
+
+
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		UserDetails user =
+			 User.builder()
+				.username("user")
+				.password(encoder.encode("password"))
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
